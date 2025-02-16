724a62a7a71950e409d320d7c864637e1ed4f298
kaicheLin <u10906123@ms.ttu.edu.tw>
2025-02-16 22:08:52 +0800

    sqlDBganarate

--- databese1.sql
+++ databese1.sql
@@ -5,6 +5,11 @@
 這是多行註解
 */
 
+# windows不分大小寫 linux 分大小寫
+# 關鍵字(CREATE, SHOW, ...)一般大寫
+# 庫名,表名 欄位名 一般小寫
+
+
 # DDL 資料定義語言（data definition language)
 # 用於明確定義概念綱要(conceptual schemas)及內部綱要(internal schemas)
 # create alter drop rename
@@ -18,15 +23,22 @@
 # commit rollback savepoint
 
 
-CREATE DATABASE DatabaseTest;   -- 建立新資料庫
-show DATABASES; -- 查看所有資料庫
-use DatabaseTest;   -- 使用/定位到資料庫
-create table tableTest(id int, name varchar(20), age int);  -- 建立新資料表 (欄位 型態)
-show tables;    -- 查看所有資料表
-insert into tableTest values (1, 'man1', 15);   -- 輸入 資料values(...) 到 資料表tableTest
-insert into tableTest values (2, '一個人', 14);
-insert into tableTest values (3, 'man2', 16);
-delete from tableTest where id = 3;     -- 刪除 資料表tableTest 欄位id=3 的內容
-select * from tableTest;    -- 查看 資料表tableTest 所有內容
+CREATE DATABASE databaseTest;   -- 建立新資料庫
+SHOW DATABASES; -- 查看所有資料庫
+USE databaseTest;   -- 使用/定位到資料庫
+CREATE TABLE tableTest(id int, name varchar(20), age int);  -- 建立新資料表 (欄位 型態)
+SHOW TABLE;    -- 查看所有資料表
+INSERT INTO tableTest values (1, 'man1', 15);   -- 輸入 資料values(...) 到 資料表tableTest
+INSERT INTO tableTest values (2, '一個人', 14);
+INSERT INTO tableTest values (3, 'man2', 16);
+DELETE FROM tableTest WHERE id = 3;     -- 刪除 資料表tableTest 欄位id=3 的內容
+SELECT * FROM tableTest;    -- 查看 資料表tableTest 所有內容
 
+
+
+USE atguigudb;
+SELECT department_id "部門id" FROM employees; -- 別名
+SELECT DISTINCT department_id FROM employees; -- DISTINCT 不重複
+SELECT DISTINCT job_id, department_id FROM employees;
+SELECT salary, commission_pct, salary * (1 + IFNULL(commission_pct, -1)) * 12  FROM employees; -- IFNULL 運算時將NULL替為,後的值
 
